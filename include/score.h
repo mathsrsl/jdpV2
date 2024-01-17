bbf72e452471c7cc32b9537de35ab0344a531821
t0ubab <147103263+t0ubab@users.noreply.github.com>
2024-01-17 20:40:53 +0100

    Update score.h

--- include/score.h
+++ include/score.h
@@ -12,10 +12,10 @@
 
 void filFile(FILE *fichier2, char nom1[TAILLE_NOM], char score1[TAILLE_SCORE], char nom2[TAILLE_NOM], char score2[TAILLE_SCORE],char nom3[TAILLE_NOM], char score3[TAILLE_SCORE]);
 void filVoidFile(FILE * fichier2, char nom[TAILLE_NOM], char score[TAILLE_SCORE]);
-void results(WINDOW * resultBox, float temps, bool game);
+int results(WINDOW * resultBox, float temps, bool game);
 
 //fonctions en fonction de game
-void winResults(WINDOW * resultBox, float temps, bool game);
+int winResults(WINDOW * resultBox, float temps, bool game);
 void looseResults(WINDOW * resultBox, bool game);
 
 //affiche simplement les resultats a la fin du jeu,
