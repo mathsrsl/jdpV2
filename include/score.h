c48ec7052830983cc96356e406a8e505c587f0a3
t0ubab <147103263+t0ubab@users.noreply.github.com>
2024-01-20 15:11:00 +0100

    Update score.h

--- include/score.h
+++ include/score.h
@@ -10,12 +10,17 @@
 #define TAILLE_NOM 6
 #define TAILLE_SCORE 10
 
+//Fonction qui va remplir un fichier avec les scores
 void filFile(FILE *fichier2, char nom1[TAILLE_NOM], char score1[TAILLE_SCORE], char nom2[TAILLE_NOM], char score2[TAILLE_SCORE],char nom3[TAILLE_NOM], char score3[TAILLE_SCORE]);
+//Fonction qui va remplir un fichier vide avec juste le score du joueur
 void filVoidFile(FILE * fichier2, char nom[TAILLE_NOM], char score[TAILLE_SCORE]);
+//Fonction qui va gérer les résultats
 int results(WINDOW * resultBox, float temps, bool game,int mode);
 
 //fonctions en fonction de game
+//Fonction si la partie est remportée
 int winResults(WINDOW * resultBox, float temps, bool game);
+//Fonction si la partie est perdue (on a appuyé sur q pour quitter)
 int looseResults(WINDOW * resultBox, bool game,int mode);
 
 //affiche simplement les resultats a la fin du jeu,
