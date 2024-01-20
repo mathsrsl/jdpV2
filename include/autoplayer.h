c100cc6c744e88e2b35eb99eb90fe8b5483c901c
t0ubab <147103263+t0ubab@users.noreply.github.com>
2024-01-20 15:09:20 +0100

    Update autoplayer.h

--- include/autoplayer.h
+++ include/autoplayer.h
@@ -13,12 +13,13 @@
 #define LARGEUR 11
 #define TAILLE_DECK 12
 
+//Fonction qui permet d'afficher le jeu en fct des cartes comparées
 void displayGameAuto (Carte *deck, Carte *compared1, Carte *compared2, int TAILLE);
-
+//Fonction qui va gérer la fin du jeu avec l'affichage des scores
 void endGame (Carte *deck, float elapsed_time, bool game, int width);
-
+//Fonction qui va comparer les valeurs de deux cartes selectionnées
 int compare(Carte *compared1, Carte *compared2, Carte *deck, int count);
-
+//Fonction du mode de jeu autoplayer
 void autoplayer(int width);
 
 #endif // AUTOPLAYER_H_INCLUDED
